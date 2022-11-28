4f66041fc7231265a3083f092bf90cec66bffb68
Dmytro Hanych <Hanych_IT@ukr.net>
2022-11-28 23:55:08 +0200

    added src

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { Container } from './styleConfig/App.styled';
+import { UserProfile } from './components/userprofile/UserProfile';
+import { Statistics } from './components/statistics/Statistics';
+import { FriendList } from './components/friendlist/FriendList';
+import { TransactionHistory } from './components/transaction/TransactionHistory';
+
+export const App = () => {
+  return (
+    <Container>
+      <UserProfile />
+      <Statistics />
+      <FriendList />
+      <TransactionHistory />
+    </Container>
+  );
+};
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,38 @@
+import PropTypes from 'prop-types';
+import {
+  StatisticsBox,
+  Title,
+  StatsticsList,
+  StatisticsItem,
+  StatisticsLabel,
+  Percentage,
+} from './Statistics.styled';
+import { getRandomHexColor } from 'helpers/getRandomColor';
+import data from 'data/data.json';
+
+export const Statistics = ({ title = 'Upload stats' }) => {
+  return (
+    <StatisticsBox>
+      {title && <Title>{title}</Title>}
+      <StatsticsList data={data}>
+        {data.map(({ id, label, percentage }) => (
+          <StatisticsItem backgroundColor={getRandomHexColor()} key={id}>
+            <StatisticsLabel>{label}</StatisticsLabel>
+            <Percentage>{percentage}%</Percentage>
+          </StatisticsItem>
+        ))}
+      </StatsticsList>
+    </StatisticsBox>
+  );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
+};
