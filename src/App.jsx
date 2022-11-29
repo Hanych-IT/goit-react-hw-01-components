342b7c8e79c3e003a04d7c4b9277779d1c79292d
Dmytro Hanych <Hanych_IT@ukr.net>
2022-11-29 19:34:17 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,25 @@ import { Container } from './styleConfig/App.styled';
 import { UserProfile } from './components/userprofile/UserProfile';
 import { Statistics } from './components/statistics/Statistics';
 import { FriendList } from './components/friendlist/FriendList';
+import data from './data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 import { TransactionHistory } from './components/transaction/TransactionHistory';
 
 export const App = () => {
   return (
     <Container>
-      <UserProfile />
-      <Statistics />
-      <FriendList />
-      <TransactionHistory />
+      <UserProfile
+        avatar={UserProfile.avatar}
+        username={UserProfile.username}
+        tag={UserProfile.tag}
+        location={UserProfile.location}
+        stats={UserProfile.stats}
+      />
+      <Statistics title="Upload stats" data={data} />
+      <Statistics data={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory itransactions={transactions} />
     </Container>
   );
 };
